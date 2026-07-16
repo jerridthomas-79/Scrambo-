3d347291b3223f6fec536efc93e4e0c47ed9275b
jerridthomas-79 <jerrid.thomas@gmail.com>
2026-07-15 21:11:22 -0500

    Load issue 2 enhancement styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/global.css";
 import "./styles/enhancements.css";
+import "./styles/issue2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
